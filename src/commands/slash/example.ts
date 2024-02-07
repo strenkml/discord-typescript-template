ee4a155a1fb399cea12b683ec369fa8f961a7358
Matt Strenk <mstrenk12@gmail.com>
2024-02-07 19:31:53 +0000

    Update slash commands

--- src/commands/slash/example.ts
+++ src/commands/slash/example.ts
@@ -1,32 +1,32 @@
-import { CommandInteraction, PermissionsBitField } from "discord.js";
+import { ChatInputCommandInteraction } from "discord.js";
 
-import SlashCommand from "../../models/SlashCommand";
+import SlashCommand, { PARAM_TYPES } from "../../models/SlashCommand";
 
+// The 'SlashCommand' could be replaced with 'AdminSlashCommand' if you wanted the command to be only accessible by administrators
 export default class Example extends SlashCommand {
   constructor() {
-    // Set the name and the descrption of the command
+    // Set the name and the description of the command
     super("example", "Example slash command");
 
     // Set additional configuration for the command if needed
-    // In this case the command is disabled in DMs, only admins
-    // can use the command, and there is a string option for the string to be returned to the caller
-    this.data
-      .setDMPermission(false)
-      .setDefaultMemberPermissions(PermissionsBitField.Flags.Administrator)
-      .addStringOption((option) =>
-        option
-          .setName("text")
-          .setDescription("Text to be returned")
-          .setRequired(true),
-      );
+    // There is a string option for the string to be returned to the caller
+    this.data.addStringOption((option) =>
+      option
+        .setName("text")
+        .setDescription("Text to be returned")
+        .setRequired(true),
+    );
   }
 
-  async execute(interaction: CommandInteraction): Promise<void> {
-    // Read in the option from the command and reply to the user with it
-    let text = interaction.options.get("text")?.value;
+  async execute(interaction: ChatInputCommandInteraction): Promise<void> {
+    // Get the value of the text string option from the interaction
+    const text: string = this.getParamValue(
+      interaction,
+      PARAM_TYPES.STRING,
+      "text",
+    );
 
-    text = text == undefined ? "" : text.toString();
-
+    // Reply to the user with the user supplied text
     interaction.reply(text);
   }
 }
--- src/models/SlashCommand.ts
+++ src/models/SlashCommand.ts
@@ -1,4 +1,9 @@
-import { CommandInteraction, SlashCommandBuilder } from "discord.js";
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import {
+  ChatInputCommandInteraction,
+  PermissionsBitField,
+  SlashCommandBuilder,
+} from "discord.js";
 
 export default abstract class SlashCommand {
   // Contains the behavior of the command
@@ -20,9 +25,80 @@ export default abstract class SlashCommand {
     // See the example slash command (src/commands/slash/example.ts)
     this.data = new SlashCommandBuilder()
       .setName(this.name)
-      .setDescription(this.description);
+      .setDescription(this.description)
+      .setDMPermission(false);
   }
 
   // Called when the command is triggered
-  abstract execute(interaction: CommandInteraction): Promise<void>;
+  abstract execute(interaction: ChatInputCommandInteraction): Promise<void>;
+
+  protected getParamValue(
+    interaction: ChatInputCommandInteraction,
+    type: PARAM_TYPES,
+    paramName: string,
+  ): any | null {
+    let val: any = undefined;
+    switch (type) {
+      case PARAM_TYPES.STRING:
+        val = interaction.options.getString(paramName);
+        break;
+      case PARAM_TYPES.ROLE:
+        val = interaction.options.getRole(paramName);
+        break;
+      case PARAM_TYPES.BOOLEAN:
+        val = interaction.options.getBoolean(paramName);
+        break;
+      case PARAM_TYPES.CHANNEL:
+        val = interaction.options.getChannel(paramName);
+        break;
+      case PARAM_TYPES.ATTACHMENT:
+        val = interaction.options.getAttachment(paramName);
+        break;
+      case PARAM_TYPES.INTEGER:
+        val = interaction.options.getInteger(paramName);
+        break;
+      case PARAM_TYPES.MEMBER:
+        val = interaction.options.getMember(paramName);
+        break;
+      case PARAM_TYPES.USER:
+        val = interaction.options.getUser(paramName);
+        break;
+    }
+    return val;
+  }
+
+  protected isSubCommand(
+    interaction: ChatInputCommandInteraction,
+    subCommandName: string,
+  ): boolean {
+    return interaction.options.getSubcommand() == subCommandName;
+  }
+
+  protected isSubCommandGroup(
+    interaction: ChatInputCommandInteraction,
+    subCommandGroupName: string,
+  ): boolean {
+    return interaction.options.getSubcommandGroup() == subCommandGroupName;
+  }
+}
+
+export abstract class AdminSlashCommand extends SlashCommand {
+  constructor(name: string, description: string) {
+    super(name, description);
+
+    this.data.setDefaultMemberPermissions(
+      PermissionsBitField.Flags.Administrator,
+    );
+  }
+}
+
+export enum PARAM_TYPES {
+  STRING,
+  INTEGER,
+  BOOLEAN,
+  ROLE,
+  CHANNEL,
+  USER,
+  MEMBER,
+  ATTACHMENT,
 }
