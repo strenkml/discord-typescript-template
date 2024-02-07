253ac28bf1d06637050e00b6cbb4650491aaa4c5
Matt Strenk <mstrenk12@gmail.com>
2024-02-07 19:22:48 +0000

    Add method to Database abstract class

--- src/providers/Database.ts
+++ src/providers/Database.ts
@@ -1,19 +1,27 @@
 import Enmap, { EnmapOptions } from "enmap";
-
 import Logger from "stumper";
 
 export default abstract class Database {
   protected db: Enmap;
   protected name: string;
 
-  constructor(protected options: EnmapOptions<undefined, undefined>) {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  constructor(protected options: EnmapOptions<any, any>) {
     this.db = new Enmap(options);
     this.name = options.name || "Database";
   }
 
   // Add methods below that will be shared across all databases
   wipe(): void {
-    Logger.info("Wiping database", this.options.name);
+    Logger.info("Wiping database", this.name);
     this.db.clear();
+  }
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  getAllKeysAndValues(): Array<{ key: string | number; value: any }> {
+    const arr = Array.from(this.db);
+    return arr.map((val) => {
+      return { key: val[0], value: val[1] };
+    });
   }
 }
